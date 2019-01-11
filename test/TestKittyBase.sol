225d1ff04c247a1cf668517bd6799ddd9749b5e1
Blaz Bagic <blaz.bagic@gmail.com>
2019-01-11 21:20:13 +0100

    Add test for all fields of function getKittyById

--- test/TestKittyBase.sol
+++ test/TestKittyBase.sol
@@ -24,4 +24,44 @@ contract TestKittyBase {
       error_message);
   }
 
+  function testGetKittyById() public {
+    KittyBase kittyBase = KittyBase(DeployedAddresses.KittyBase());
+    
+    uint256 geneticCode = 213312351234;
+    uint32 parent1_id = 1;
+    uint32 parent2_id = 2;
+    uint16 generation = 3;
+
+    uint32 kittyId = uint32(
+      kittyBase.createKitty(
+        DeployedAddresses.KittyBase(), // passing the contract itself as owner
+        geneticCode, parent1_id, parent2_id, generation
+      )
+    );
+
+    ( uint _geneticCode,
+      uint _parent1_id,
+      uint _parent2_id,
+      uint _generation ) = kittyBase.getKittyById(kittyId);
+    
+    string memory error_message = "Function getKittyById returned unexpected value(s).";
+
+    Assert.equal(
+      uint(geneticCode),
+      _geneticCode,
+      error_message);
+    Assert.equal(
+      uint(parent1_id),
+      _parent1_id,
+      error_message);
+    Assert.equal(
+      uint(parent2_id),
+      _parent2_id,
+      error_message);
+    Assert.equal(
+      uint(generation),
+      _generation,
+      error_message);
+  }
+
 }
