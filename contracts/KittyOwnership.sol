1c2da7fc8e786120a73668928b4f6148dfcac43d
Blaz Bagic <blaz.bagic@gmail.com>
2019-01-16 16:20:59 +0100

    Initialized npm in this project and installed the openzepplin framework

--- contracts/KittyOwnership.sol
+++ contracts/KittyOwnership.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "./KittyBase.sol";
-import "./ERC721.sol";
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
 contract KittyOwnership is KittyBase, ERC721 { 
 	//TODO 
