cbe17982f7a37246049091d8819196c1c6ef5042
Blaz Bagic <blaz.bagic@gmail.com>
2019-01-23 19:15:50 +0100

    Fix for buying kitties

--- contracts/KittyMarket.sol
+++ contracts/KittyMarket.sol
@@ -3,25 +3,29 @@ pragma solidity ^0.5.0;
 import "./KittyOwnership.sol";
 
 contract KittyMarket is KittyOwnership {
-    mapping (uint32 => uint32) public kittyIndexToPrice;
+  /* Price of 0 means kitty is not for sale */
+  mapping (uint32 => uint256) public kittyIndexToPrice;
 
-    function putKittyUpForSale(uint32 kittyId, uint32 price) external {
-      require(ownerOf(kittyId) == msg.sender);
-      kittyIndexToPrice[kittyId] = price;
-    }
+  function putKittyUpForSale(uint32 kittyId, uint256 price) external {
+    require(ownerOf(kittyId) == msg.sender);
+    kittyIndexToPrice[kittyId] = price;
+  }
 
-    function cancelSale(uint32 kittyId) external {
-      require(ownerOf(kittyId) == msg.sender);
-      delete kittyIndexToPrice[kittyId];
-    }
+  function cancelSale(uint32 kittyId) external {
+    require(ownerOf(kittyId) == msg.sender);
+    kittyIndexToPrice[kittyId] = 0;
+  }
 
-    function buyKitty(uint32 kittyId) external payable {
-      uint32 price = kittyIndexToPrice[kittyId];
-      
-      require(price >= 0);
-      require(msg.value >= price);
+  function buyKitty(uint32 kittyId) external payable {
+    uint256 price = kittyIndexToPrice[kittyId];
+    address payable owner = address(uint160(ownerOf(kittyId)));
 
-      delete kittyIndexToPrice[kittyId];
-      safeTransferFrom(ownerOf(kittyId), msg.sender, kittyId);
-    }
+    require(owner != msg.sender);
+    require(price > 0);
+    require(msg.value >= price);
+
+    kittyIndexToPrice[kittyId] = 0;
+    _transferFrom(ownerOf(kittyId), msg.sender, kittyId);
+    owner.transfer(msg.value);
+  }
 }
